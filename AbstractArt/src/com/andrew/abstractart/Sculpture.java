468682e8d4be04587069bac6ffa5e201577b5acc
Andrew T Tran <me@andrewtran-shi.dev>
2023-08-06 11:15:52 -0700

    modified:   AbstractArt/bin/com/andrew/abstractart/Sculpture$Museum.class 	modified:   AbstractArt/src/com/andrew/abstractart/Sculpture.java

--- AbstractArt/src/com/andrew/abstractart/Sculpture.java
+++ AbstractArt/src/com/andrew/abstractart/Sculpture.java
@@ -18,14 +18,14 @@ class Sculpture extends Art {
 	  }
 	  
 	  public class Museum {
-		  public static void main(String[] args) {
-		    Painting painting1 = new Painting("Starry Night", "Vincent van Gogh", "Oil on canvas", "Oil");
-		    Painting painting2 = new Painting("The Persistence of Memory", "Salvador Dali", "Oil on canvas", "Oil");
-		    Painting painting3 = new Painting("The Scream", "Edvard Munch", "Oil, tempera, and pastel on cardboard", "Oil");
+		public static void main(String[] args) {
+			new Painting("Starry Night", "Vincent van Gogh", "Oil on canvas", "Oil");
+			new Painting("The Persistence of Memory", "Salvador Dali", "Oil on canvas", "Oil");
+			new Painting("The Scream", "Edvard Munch", "Oil, tempera, and pastel on cardboard", "Oil");
 
-		    Sculpture sculpture1 = new Sculpture("David", "Michelangelo", "Marble statue of David", "Marble");
-		    Sculpture sculpture2 = new Sculpture("The Thinker", "Auguste Rodin", "Bronze statue of a man in deep thought", "Bronze");
-		  }
+			new Sculpture("David", "Michelangelo", "Marble statue of David", "Marble");
+			new Sculpture("The Thinker", "Auguste Rodin", "Bronze statue of a man in deep thought", "Bronze");
+		}
 		}
 	  
 	}
